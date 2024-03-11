2ce3bdb7f6e71d1efbae6774df4b63113aafa23c
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-03-11 23:42:12 +0900

    [Feat] 회원가입에 필요한 기능을 담은 인터페이스 작성
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>

---
+++ src/main/java/com/example/board/member/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package com.example.board.member.repository;
+
+import com.example.board.member.domain.Member;
+
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+
+    void delete(Member member);
+
+    Optional<Member> findByIdAndDeletedAtIsNull(long id);
+
+    Optional<Member> findByEmailAndDeletedAtIsNull(String email);
+
+    Optional<Member> findByUserIdAndDeletedAtIsNull(String id);
+}
